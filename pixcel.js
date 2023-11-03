0f75528fb2ca045ccf914fcb753d76be1a5ef6e5
Edimeh-SundayGoodness <edimehgoodness@gmail.com>
2023-11-03 13:09:30 +0100

    The rows and columns are now working

---
+++ pixcel.js
@@ -0,0 +1,81 @@
+// mock up the labels and input display
+// create two buttons to draw and clear your table
+// give buttons value property equal to the display
+// give buttons and inputs similar class
+// grab them to the js file
+// create variables that will hold the input values of your labels
+// for the number labels change them to numbers using the number constructor or parselnt
+// add an event listener of click to your draw button\
+// create variables for your rows and columns
+// using for loop for your row variable, create tr element
+// inside the for loop,create another for loop for the columnn
+// inside the for loop for the column, create the td Element
+// outside the for loop append your td to your table row
+// append your table row to your div or table
+// add an event listener of click to the clearButton
+// set the divContainer innerHTML to empty string to clear the tableContainer
+
+
+// let inputRow = document.getElementById('row')
+// let inputColumn = document.getElementById('column')
+let colorButton = document.getElementById('color')
+let drawButton = document.getElementById('draw')
+let clearButton = document.getElementById('clear')
+let divContainer = document.getElementById('table')
+let eraseBtn = document.getElementById('erase');
+let paintBtn = document.getElementById('paint')
+let cell = ''
+
+erase = false
+paint = false
+
+function click(){
+    let Row = parseInt(document.getElementById('row').value)
+    let Column = parseInt(document.getElementById('column').value)
+    let tableContainer = document.createElement('table') 
+    
+    for(let i=0; i<Row; i++){
+        let trow = document.createElement('tr')
+        for(let a=0; a<Column; a++){
+            let tdata = document.createElement('td')
+            // tdata.innerHTML = '  '
+            // tdata.style.backgroundColor = 'white'
+            // tdata.style.width= '1em'
+            // tdata.style.height= '1em'
+            // tdata.style.border = '3px solid black'
+            // tdata.setAttribute("class", 'tdata');
+
+            // tdata.addEventListener('mousedown', () =>{
+            //     paint = true
+            //     if(erase) {
+            //         tdata.style.backgroundColor = 'transparent'
+            //     }else{
+            //         tdata.style.backgroundColor = colorButton.value
+            //     }
+            // })
+            // tdata.addEventListener('mouseup', () =>{
+            //     paint = false
+            // })
+            console.log("na me")
+            trow.appendChild(tdata)
+            tableContainer.appendChild(trow)
+        }
+        divContainer.innerHTML = ""
+        divContainer.appendChild(tableContainer)
+    }
+    console.log(tableContainer)
+    // divContainer.appendChild(tableContainer)
+}
+drawButton.addEventListener('click', click)
+
+clearButton.addEventListener('click', (event) =>{ 
+    divContainer.innerHTML = ''
+    event.preventDefault()
+})
+
+// eraseBtn.addEventListener('click', () =>{
+//     erase = true
+// })
+// paintBtn.addEventListener('click', () =>{
+//     erase = false
+// })
