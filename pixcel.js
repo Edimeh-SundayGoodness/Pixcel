6ab75b3bb8d15cab92dc62d1ac879f5b72009603
Edimeh-SundayGoodness <edimehgoodness@gmail.com>
2023-11-20 20:28:14 +0100

    i have finished my pixcel project

--- pixcel.js
+++ pixcel.js
@@ -32,9 +32,14 @@ let divContainer = document.getElementById('table')
 let eraseBtn = document.getElementById('erase');
 let paintBtn = document.getElementById('paint')
 let cell = ''
+let stringans = ''
+let data = {};
+let count;
 
 erase = false
 paint = false
+// Row.innerHTML = 0
+// Column.innerHTML = 0
 
 function click(){
     let Row = parseInt(document.getElementById('row').value)
@@ -43,6 +48,7 @@ function click(){
     
     for(let i=0; i<Row; i++){
         let trow = document.createElement('tr')
+        
         for(let a=0; a<Column; a++){
             let tdata = document.createElement('td')
             // tdata.innerHTML = '  '
@@ -63,6 +69,28 @@ function click(){
             tdata.addEventListener('mouseup', () =>{
                 paint = false
             })
+            tdata.addEventListener('dblclick', () =>{
+                tdata.style.backgroundColor = 'transparent'
+            })
+            // tdata.addEventListener('click', () =>{
+            //     count = 0;
+            //     tdata.style.backgroundColor = 'transparent'
+            //     if(count < 2){
+            //         tdata.style.backgroundColor = colorButton.value
+            //     }else if(count > 1){
+            //         tdata.style.backgroundColor = 'yellow'
+            //     }count++
+                
+            // })
+            //     count++
+            //     if(count > 2){
+            //         tdata.style.backgroundColor = 'yellow'
+            //     }
+                // else if(count > 1){
+                //     tdata.style.backgroundColor = colorButton.value
+                // }
+                // count = 0
+            
             console.log("na me")
             trow.appendChild(tdata)
             tableContainer.appendChild(trow)
@@ -85,4 +113,36 @@ eraseBtn.addEventListener('click', () =>{
 })
 paintBtn.addEventListener('click', () =>{
     erase = false
+})
+drawButton.addEventListener('click', () =>{
+    let Row = parseInt(document.getElementById('row').value)
+    let Column = parseInt(document.getElementById('column').value)
+    let colorValue = colorButton.value
+    
+    let data = {inputRows:Row, inputColumn:Column, color: colorValue}
+
+    if(localStorage.getItem('count')){
+        count = Number(localStorage.getItem('count'));
+    }else{
+        count = 0
+    }
+
+    localStorage.setItem(`data${count}`, JSON.stringify(data))
+
+    count++
+    localStorage.setItem('count',count)
+
+    // td1.innerText = `${retrievedData.firstname}`;
+    // td2.innerText = `${retrievedData.lastname}`;
+    // td3.innerText = `${retrievedData.age}`;
+
+    for(a = 0; a < localStorage.length; a++){
+        let retrievedData = JSON.parse(localStorage.getItem(`data${a}`));
+    
+        Row.value = `${retrievedData.inputRows}`
+        Column.value = `${retrievedData.inputColumn}`
+        colorValue.value = `${retrievedData.color}`
+    }
+
+
 })
